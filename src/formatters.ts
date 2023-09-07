88fc12ae3499a6b0883866b9e94614803bd8e390
Ng'etich Stephen <stephenngetich5@gmail.com>
2023-09-08 01:36:04 +0300

    feat: add date format validator

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,10 +1,50 @@
 import { differenceInMilliseconds, format } from 'date-fns';
 
+/**
+ * Checks if a given date format string is valid.
+ *
+ * This function checks if the input date format string contains only valid characters.
+ * Valid characters include: y, G, u, q, Q, M, L, w, d, e, E, c, a, b, B, h, H, k, K, m, s, S, z, O, x, X.
+ * Valid characters are based on [date-fns unicode format](https://date-fns.org/v2.30.0/docs/format).
+ *
+ * @param {string} dateFormat - The date format string to validate.
+ * @returns {boolean} Returns true if the format is valid.
+ * @throws {Error} Throws an error if the format contains invalid characters.
+ */
+export const isValidDateFormat = (dateFormat: string) => {
+  const validChars = 'yGuqQMLwdeEcabBhHkKmsSzOxX';
+
+  const invalidChars = Array.from(dateFormat).filter(
+    (char) => !validChars.includes(char)
+  );
+
+  if (invalidChars.length > 0) {
+    throw new Error(
+      `Invalid characters in date format: ${invalidChars.join(
+        ', '
+      )} . see https://date-fns.org/docs/format for valid characters`
+    );
+  }
+
+  return true; // The format is valid.
+};
+
 export const getTimeString = (
   date: Date,
   dateFormat = 'yyy-MM-dd HH:mm:ss'
 ): string => {
-  return format(date, dateFormat);
+  try {
+    // Check if the dateFormat is valid before formatting
+    isValidDateFormat(dateFormat);
+
+    return format(date, dateFormat);
+  } catch (error) {
+    if (error instanceof RangeError) {
+      console.error(error.message);
+    } else {
+      throw error; // Re-throw other errors for further handling
+    }
+  }
 };
 
 export const getTimeDiff = (end: Date, start: Date): string => {
