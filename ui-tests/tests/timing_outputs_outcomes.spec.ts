cb935d0ced995ee4b535ff416c5bc8cbd30c6bc2
krassowski <5832902+krassowski@users.noreply.github.com>
2024-04-26 12:51:28 +0100

    Do not test execution started again, remove unused import and cells

--- ui-tests/tests/timing_outputs_outcomes.spec.ts
+++ ui-tests/tests/timing_outputs_outcomes.spec.ts
@@ -1,9 +1,9 @@
 import { expect, galata, test } from '@jupyterlab/galata';
-import { openNotebook, cleanup, acceptDialog, maskedScreenshot } from './utils';
+import { openNotebook, cleanup, maskedScreenshot } from './utils';
 
 const SETTINGS_ID = 'jupyterlab-execute-time:settings';
 
-test.describe('Timing outcomes', () => {
+test.describe('Timing outcomes with ', () => {
   test.beforeEach(openNotebook('Timing_outputs_outcomes.ipynb'));
   test.afterEach(cleanup);
   // Disable flashing highlight for screenshot consistency
@@ -18,21 +18,8 @@ test.describe('Timing outcomes', () => {
     },
   });
 
-  test('"Execution started at" state', async ({ page }) => {
-    const cell = await page.notebook.getCell(2);
-    // Start executing cell but do not wait for it to complete
-    await cell.click();
-    await page.keyboard.press('Control+Enter');
-
-    const widget = await cell.waitForSelector('.execute-time');
-    expect(await widget.textContent()).toContain('Execution started at');
-    expect(await maskedScreenshot(widget)).toMatchSnapshot(
-      'execution-started-outputs.png'
-    );
-  });
-
   test('"Last executed at" state', async ({ page }) => {
-    const cell = await page.notebook.getCell(4);
+    const cell = await page.notebook.getCell(2);
 
     // Execute cell and wait for it to complete
     await page.notebook.runCell(4);
@@ -40,6 +27,8 @@ test.describe('Timing outcomes', () => {
     const widget = await cell.waitForSelector('.execute-time');
     expect(await widget.textContent()).toContain('Last executed at');
     expect(await widget.textContent()).toContain('outputs at');
-    expect(await maskedScreenshot(widget)).toMatchSnapshot('last-executed-outputs.png');
+    expect(await maskedScreenshot(widget)).toMatchSnapshot(
+      'last-executed-outputs.png'
+    );
   });
 });
