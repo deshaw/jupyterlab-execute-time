a79b72e1346197afa7fdd95b897dc7293fd90dcd
krassowski <5832902+krassowski@users.noreply.github.com>
2024-05-16 13:55:57 +0100

    Add a test for extension showing up in the Settings Editor right away

---
+++ ui-tests/tests/settings_editor.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@jupyterlab/galata';
+
+test.describe('Settings Editor', () => {
+  test('Execute Time show up in the Settings Editor', async ({ page }) => {
+    await page.evaluate(async () => {
+      await window.jupyterapp.commands.execute('settingeditor:open');
+    });
+    const plugin = page.locator(
+      '.jp-PluginList .jp-PluginList-entry >> text="Execute Time"'
+    );
+
+    expect(plugin).toHaveCount(1);
+  });
+});
